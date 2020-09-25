5a1ba542c664f54fddd4dd73afe4af466ed7363a
Nikhil Kumar Yadav <nikhilyadavindigo@gmail.com>
2020-09-25 23:00:58 +0530

    helloWorld

---
+++ PracticeCapg/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace PracticeCapg
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
